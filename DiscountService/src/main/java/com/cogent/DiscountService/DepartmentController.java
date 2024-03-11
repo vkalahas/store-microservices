cd543217772c5632f26407b317ee4ba268d5f0a6
vkalahas <vkalahas@ucsd.edu>
2024-03-11 14:09:58 -0700

    maybe this works

--- DiscountService/src/main/java/com/cogent/DiscountService/DepartmentController.java
+++ DiscountService/src/main/java/com/cogent/DiscountService/DepartmentController.java
@@ -1,4 +1,4 @@
-package com.cogent.DepartmentService;
+package com.cogent.DiscountService;
 
 import java.util.List;
 import java.util.Optional;
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,26 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/departments")
+@RequestMapping("/api/discount")
 public class DepartmentController {
 	
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-	@Autowired
-	private DepartmentService deptService;
-	
-	@GetMapping
-	public List<Department> getAllDepartments() {
-		return deptService.getAllDepartments();
-	}
-	
-	@GetMapping("/{id}")
-	public Optional<Department> getDepartmentById(@PathVariable("id") int id) {
-		return deptService.getDepartmentById(id);
-	}
-	
-	@PostMapping
-	public Department saveDepartment(@RequestBody Department obj) {
-		return deptService.saveDepartment(obj);
+	@GetMapping()
+	public ResponseEntity<Integer> getDiscount(@RequestBody String order) {
+		
+		int discountValue = (int) Math.random() * 10;
+		
+		// edit later maybe
+		return ResponseEntity.status(HttpStatus.OK).body(discountValue);
 	}
 	
 }
