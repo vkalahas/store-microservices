31abd740fd93ffb8d74505b08640c391e79935bb
Uday Raghavan <63931374+idkuday@users.noreply.github.com>
2024-03-11 17:56:49 -0500

    asdf

---
+++ ReportsService/src/main/java/com/furniturecloud/reports/ReportsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.furniturecloud.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsServiceApplication.class, args);
+	}
+
+}
